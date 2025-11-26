ce7b570e67443e5584733dc8fa34771e4e9eab19
Pavel Leushin <leushinps@outlook.com>
2025-11-27 01:04:05 +0500

    Task0

--- Tyuiu.LeushinP.Sprint4.Task0.V28.Test/DataServiceTest.cs
+++ Tyuiu.LeushinP.Sprint4.Task0.V28.Test/DataServiceTest.cs
@@ -16,7 +16,7 @@ namespace Tyuiu.LeushinP.Sprint4.Task0.V28.Test
             DataService ds = new DataService();
             int[] array = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
             int wait = 9216;
-
+            
             var rez = ds.GetMultEvenArrEl(array);
             Assert.AreEqual(wait, rez);
         }
